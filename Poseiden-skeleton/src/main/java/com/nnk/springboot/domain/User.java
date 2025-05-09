eb3615a482fce6afc19a5083b507297c89a23dc6
QuentinTL <q.thibaut.lazaro96@gmail.com>
2025-05-09 11:49:41 +0200

    Added Arguments to Entitys and Domains

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/User.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/User.java
@@ -4,6 +4,15 @@ package com.nnk.springboot.domain;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "users")
 public class User {
@@ -18,44 +27,4 @@ public class User {
     private String fullname;
     @NotBlank(message = "Role is mandatory")
     private String role;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFullname() {
-        return fullname;
-    }
-
-    public void setFullname(String fullname) {
-        this.fullname = fullname;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
 }
