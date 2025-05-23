3634add9b3a669e1acdb15e18e7f60f409175526
QuentinTL <q.thibaut.lazaro96@gmail.com>
2025-05-23 14:59:58 +0200

    Update BidListService.java
    Create BidListService and update method to use it in controller

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/services/BidListService.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/services/BidListService.java
@@ -1,5 +1,54 @@
 package com.nnk.springboot.services;
 
-public class BidListService {
+import com.nnk.springboot.domain.BidList;
+import com.nnk.springboot.repositories.BidListRepository;
+import com.nnk.springboot.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+public class BidListService {
+    @Autowired
+    BidListRepository bidListRepository;
+
+    public BidList save(BidList bid){
+
+        BidList newBid = new BidList();
+        newBid.setAccount(bid.getAccount());
+        newBid.setType(bid.getType());
+        newBid.setBidQuantity(bid.getBidQuantity());
+
+        return bidListRepository.save(newBid);
+    }
+
+    public BidList update(Integer id, BidList bidList){
+
+        var updatedBid = bidListRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("No Bid to update with id : " + id));
+
+        updatedBid.setAccount(bidList.getAccount());
+        updatedBid.setType(bidList.getType());
+        updatedBid.setBidQuantity(bidList.getBidQuantity());
+
+        return bidListRepository.saveAndFlush(updatedBid);
+    }
+
+
+    public void delete(Integer id) {
+        BidList bid = bidListRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("No bid to delete with this id : " + id));
+
+        bidListRepository.delete(bid);
+    }
+
+    public BidList findById(Integer id) {
+        return bidListRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("No bid founded with this id : " + id));
+    }
+
+    public List<BidList> findAll() {
+        return bidListRepository.findAll();
+    }
 }
