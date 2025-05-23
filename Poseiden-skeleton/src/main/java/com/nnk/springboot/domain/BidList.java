fd47044fd5f738017d5f49281975f86c2bc8ab62
QuentinTL <q.thibaut.lazaro96@gmail.com>
2025-05-23 14:57:32 +0200

    Update BidList.java
    Update BidList object to match with dataBase

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/BidList.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/BidList.java
@@ -1,6 +1,9 @@
 package com.nnk.springboot.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,12 +20,20 @@ import java.sql.Timestamp;
 public class BidList {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
-    private Integer BidListId;
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "bid_list_id")
+    private Integer bidListId;
 
+    @NotEmpty(message = "Account is mandatory")
     private String account;
+
+    @NotEmpty(message = "Type is mandatory")
     private String type;
+
+    @NotNull(message = "Bid quantity is mandatory")
+    @Positive(message = "Bid quantity must be positive")
     private Double bidQuantity;
+
     private Double askQuantity;
     private Double bid;
     private Double ask;
